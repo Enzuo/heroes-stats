3711a92e8677c09685ef694a5a0b74dd760d8185
Enzuo <potc.zon.e+git@gmail.com>
2021-06-23 02:00:46 +0800

    Add fontawesome icons for team members

--- src/common/components/Team.tsx
+++ src/common/components/Team.tsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import Hero from '@/common/components/Hero'
 import type {TeamType} from '@/common/types/game'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 function Team ({team, onClick} : TeamProps) {
-  const heroList = team.heroes.map((a, i) => <Hero key={i} name={a}></Hero>)
+  const heroList = team.heroes.map((a, i) => <TeamMember key={i} member={a}></TeamMember>)
   return (
     <div onClick={() => onClick()}>
       {team.label}
       {heroList}
+    </div>
+  )
+}
+
+function TeamMember({member}) {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faTimesCircle} />
+      <Hero name={member}></Hero>
+      <FontAwesomeIcon icon={faThumbsUp} />
+      <FontAwesomeIcon icon={faThumbsDown} />
     </div>
   )
 }
