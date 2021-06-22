b38d55a6acdb5cad03981e43abac0946072f8e8e
Enzuo <potc.zon.e+git@gmail.com>
2021-06-22 22:02:37 +0800

    Add styled component

--- src/common/components/Hero.js
+++ src/common/components/Hero.js
@@ -1,8 +1,27 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const HeroImg = styled.img`
+border-radius: 32px;
+height:64px;
+width:64px;
+border:#ffc500 1px solid;
+opacity:0.8;
+cursor:pointer;
+&:hover {
+  border:#416cff 1px solid;
+  opacity:1;
+}
+`
+
 
 function Hero ({name}) {
   return (
-    <img src={'img/' + name + '_Hero_Portrait.png'}/>
+    <HeroImg 
+      src={'img/' + name.replace(/ /g, '_') + '_Hero_Portrait.png'}
+      alt={name}
+      title={name}
+    />
   )
 }
 
