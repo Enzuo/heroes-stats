1c93a0a46c029eebfc5eac55e970fef23a45360c
Enzuo <potc.zon.e+git@gmail.com>
2021-06-27 11:56:32 +0800

    Recreate schema

---
+++ prisma/migrations/20210627035542_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "type" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GameHero" (
+    "id" SERIAL NOT NULL,
+    "heroId" INTEGER NOT NULL,
+    "gameId" INTEGER NOT NULL,
+    "team" INTEGER NOT NULL,
+    "isUser" BOOLEAN NOT NULL DEFAULT false,
+    "impact" INTEGER,
+    "synergy" INTEGER,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "GameHero" ADD FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE CASCADE ON UPDATE CASCADE;
