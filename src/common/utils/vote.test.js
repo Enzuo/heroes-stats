0997e6099e811713b03aad5d70883c39e36c14ed
Enzuo <potc.zon.e+git@gmail.com>
2021-07-02 01:14:53 +0800

    Calculate votes by sorting heroes instead of score

--- src/common/utils/vote.test.js
+++ src/common/utils/vote.test.js
@@ -10,9 +10,7 @@ test('test simple vote ladder', () => {
     {heroes : [{id:2, isPicked: true },{id:4, isPicked: false},{id:6, isPicked: false}]}, // 6 picked over 2
   ]
 
-  expect(calculateRankFromVotes(votes)).toStrictEqual(
-    [[1, 10], [6, 5], [2, 5], [5, 0], [4, 0], [3, 0]]
-  )
+  expect(calculateRankFromVotes(votes)).toStrictEqual([ 5, 4, 6, 3, 2, 1 ])
 });
 
 test('test tricky vote ladder', () => {
@@ -24,5 +22,14 @@ test('test tricky vote ladder', () => {
     {heroes : [{id:2, isPicked: false},{id:3, isPicked: false},{id:5, isPicked: true}]}, // 5 picked over 2
   ]
   
-  expect(calculateRankFromVotes(votes)).toBe(3)
+  expect(calculateRankFromVotes(votes)).toStrictEqual([ 4, 3, 2, 5, 1 ])
+});
+
+test('test getting on the very top of the vote ladder', () => {
+  const votes = [
+    {heroes : [{id:1, isPicked: true },{id:2, isPicked: false},{id:3, isPicked: false}]},
+    {heroes : [{id:4, isPicked: true },{id:1, isPicked: false},{id:2, isPicked: false}]},
+  ]
+
+  expect(calculateRankFromVotes(votes)).toStrictEqual([ 3, 2, 1, 4 ])
 });
--- src/common/utils/vote.tsx
+++ src/common/utils/vote.tsx
@@ -1,21 +1,35 @@
 import type * as T from '@/common/types/game'
 
 export function calculateRankFromVotes(votes : T.VoteRound[]) {
-  const totalVotes = votes.reduce((arr, r) => {
-    for(var i=0; i < r.heroes.length; i++) {
-      var h = r.heroes[i]
-      var score = h.isPicked ? 10 : 0
-      if(!arr[h.id]){
-        arr[h.id] = [score, 1]
+  const heroesLadder = votes.reduce((ladder, vote) => {
+    const pickedHero = vote.heroes.reduce((arr, h) => h.isPicked ? arr : h.id, null)
+
+    const heroesWithPos = vote.heroes.map(h => { return {
+      id : h.id, 
+      index : ladder.findIndex(l => l === h.id), 
+      isPicked : h.isPicked
+    }}).sort((a,b) => (b.isPicked ? 1 : 0) - (a.isPicked ? 1 : 0)) // first one is the picked one
+    const allPos = heroesWithPos.map(a => a.index)
+    const highestPos = Math.max(...allPos)
+
+    for(var i=0; i<heroesWithPos.length; i++){
+      var hero = heroesWithPos[i]
+
+      // hero has to be moved up
+      if(hero.isPicked && highestPos !== hero.index){
+        ladder.splice(highestPos+1, 0, hero.id)
+        if(hero.index >= 0){
+          ladder.splice(hero.index, 1)
+        }
       }
-      else {
-        arr[h.id][0] += score
-        arr[h.id][1] += 1
+
+      // hero not yet in the lader
+      else if(hero.index < 0){
+        ladder.unshift(hero.id)
       }
     }
-    return arr
-  }, {})
+    return ladder
+  }, [])
 
-  const mappedHeroes = Object.entries(totalVotes).map(a => [parseInt(a[0]), a[1][0]/a[1][1]] ) // score/nb occurences
-  return mappedHeroes.sort((a, b) => a[1] < b[1] ? 1 : -1 )
+  return heroesLadder
 }
