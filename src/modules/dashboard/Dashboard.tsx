ff9551cec0cbdd0ba7540ca2679ee61e390e57f6
Enzuo <potc.zon.e+git@gmail.com>
2021-07-06 00:52:19 +0800

    Tweak dashboard panels colors

--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -61,15 +61,16 @@ const HeroesTiles = styled.div`
 `
 
 const Panel = styled.div`
-  background:#000e27;
+  /* background:#000e27; */
   width: fit-content;
   padding: 25px;
   margin: 20px 0;
+  background: linear-gradient(45deg, #10204c, #000b1f);
   box-shadow: 0px 0px 2px #00276d;
 
   h2 {
     margin-bottom:15px;
-    color:#3a6ab7;
+    color:#5b7db5;
   }
 `
 
