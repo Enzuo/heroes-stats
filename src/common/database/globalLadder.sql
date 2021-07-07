e38e4dc77dd6ba6a2ce16dffdf1d3395f1d81595
Enzuo <potc.zon.e+git@gmail.com>
2021-07-07 23:06:36 +0800

    Format sql file

--- src/common/database/globalLadder.sql
+++ src/common/database/globalLadder.sql
@@ -7,10 +7,9 @@ WITH filter_user AS (
   FROM "VoteRound"
   GROUP BY "userId"
 )
-,votes AS (
+, votes AS (
   SELECT  
-    "HeroVote"."heroId"
-  -- 	,"HeroVote"."voteId"
+     "HeroVote"."heroId"
     ,"HeroVote"."isPicked"
     ,"VoteRound"."userId"
     ,"VoteRound"."createdAt"
@@ -21,39 +20,34 @@ WITH filter_user AS (
                         AND filter_user."count" > 30 -- user with enough vote
                         AND filter_user."elapsedTimeMed" > 1000 -- user who has spent some time thinking
 )
-
 , hero_count AS (
-	SELECT 
-		 "heroId"
-		,"userId"
-		,COUNT(*) AS "countRound"
-		,SUM("isPicked"::INT) AS "countPicked"
-	FROM votes
-	GROUP BY "heroId", "userId"
+  SELECT 
+     "heroId"
+    ,"userId"
+    ,COUNT(*) AS "countRound"
+    ,SUM("isPicked"::INT) AS "countPicked"
+  FROM votes
+  GROUP BY "heroId", "userId"
 )
 
 , hero_pick_rate_by_user AS (
-	SELECT
-	   "heroId"
-	  ,"userId"
-  	,"countPicked"::NUMERIC / "countRound"::NUMERIC AS "pickRate"
-	FROM hero_count
+  SELECT
+     "heroId"
+    ,"userId"
+    ,"countPicked"::NUMERIC / "countRound"::NUMERIC AS "pickRate"
+  FROM hero_count
 )
 , hero_pick_rate_sum AS (
-	SELECT 
-		"heroId"
-		,SUM("pickRate") AS "pickRateSum"
-		,COUNT(*) AS "count"
-	FROM hero_pick_rate_by_user
-	GROUP BY "heroId"
+  SELECT 
+    "heroId"
+    ,SUM("pickRate") AS "pickRateSum"
+    ,COUNT(*) AS "count"
+  FROM hero_pick_rate_by_user
+  GROUP BY "heroId"
 )
 
 SELECT 
    "heroId"
-	,"pickRateSum" / "count" AS "pickRate"
+  ,"pickRateSum" / "count" AS "pickRate"
 FROM hero_pick_rate_sum
 ORDER BY "pickRate" DESC
-
--- SELECT * FROM "HeroVote"
--- INNER JOIN "VoteRound" ON "VoteRound"."id" = "HeroVote"."voteId"
---  WHERE "heroId" = 29
